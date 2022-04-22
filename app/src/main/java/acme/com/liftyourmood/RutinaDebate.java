c628ba0bb5a35663fa8190699158a9269dce4c6e
alber1989 <alber1989@hotmail.com>
2022-04-22 17:12:22 +0200

    220422B

--- app/src/main/java/acme/com/liftyourmood/RutinaDebate.java
+++ app/src/main/java/acme/com/liftyourmood/RutinaDebate.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class RutinaDebate extends AppCompatActivity {
+
+    private ArrayList<String> distorsionesSeleccionadasArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +35,51 @@ public class RutinaDebate extends AppCompatActivity {
         Intent intent = getIntent();
         String[] distorsionesSeleccionadas = intent.getStringArrayExtra(MenuSeleccionDistorsiones.DISTORSIONES_SELECCIONADAS);
 
+        distorsionesSeleccionadasArrayList = new ArrayList<String>(Arrays.asList(distorsionesSeleccionadas));
+
+        Iterator itr = distorsionesSeleccionadasArrayList.iterator();
+        while (itr.hasNext())
+        {
+            String x = (String) itr.next();
+            if (x.equals(""))
+                itr.remove();
+        }
+
+
+
+
+
+
+
+/*
+
+        for(int i = 0; i< distorsionesSeleccionadas.length; i++){
+
+            if(distorsionesSeleccionadas[i].equals("")) i++;
+            if(i == distorsionesSeleccionadas.length) return;
+
+            distorsionesSeleccionadasArrayList.add(distorsionesSeleccionadas[i]);
+
+        }*/
+
+
+
+
+        AdaptadorDistorsiones adaptador = new AdaptadorDistorsiones(this);
+        ListView lv1 = findViewById(R.id.listaDistorsionesSeleccionadas);
+        lv1.setAdapter(adaptador);
+
+
+
+
+
         //for  (int i=0; i<distorsionesSeleccionadas.length; i++)
         //           Toast.makeText(this, distorsionesSeleccionadas[i], Toast.LENGTH_SHORT).show();
 
         //Toast.makeText(this, nombreUsuario, Toast.LENGTH_SHORT).show();
 
 
-
+    /*
         if(distorsionesSeleccionadas != null) {
             for(int i = 0; i<distorsionesSeleccionadas.length;i++){
 
@@ -58,9 +107,29 @@ public class RutinaDebate extends AppCompatActivity {
 
 
 
-            }
+            }*/
 
 
+    }
+
+    class AdaptadorDistorsiones extends ArrayAdapter<String> {
+
+        AppCompatActivity appCompatActivity;
+
+        AdaptadorDistorsiones(AppCompatActivity context) {
+            super(context, R.layout.distorsion_seleccionada, distorsionesSeleccionadasArrayList);
+            appCompatActivity = context;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = appCompatActivity.getLayoutInflater();
+            View item = inflater.inflate(R.layout.distorsion_seleccionada ,null);
+
+            TextView textView1 = item.findViewById(R.id.textViewDistorsion);
+            textView1.setText(distorsionesSeleccionadasArrayList.get(position));
+
+            return(item);
+        }
     }
 
 
